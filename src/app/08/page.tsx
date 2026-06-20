8addcd80c0a9e770e695ddb48eaa7a7c3f2b3890
AkkalDhami <dhamiakkal21@gmail.com>
2026-06-20 08:03:22 +0545

    feat: add page component with SVG line animation

---
+++ src/app/08/page.tsx
@@ -0,0 +1,10 @@
+import { SVGLineAnimation } from "@/components/svg-line-animation";
+
+export default function page() {
+ 
+  return (
+    <div className="flex flex-col items-center justify-center gap-12 bg-neutral-950 py-20">
+      <SVGLineAnimation />
+    </div>
+  );
+}
